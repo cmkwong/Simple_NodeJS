10aa02d3dce8dd19d05223a755a47508efd3b037
cmkwong <cheung.mankwong@yahoo.com.hk>
2020-09-27 14:29:15 +0800

    (15)1. read the template\n 2. replace the key word into html code for Overview\n 3. used map function

--- index.js
+++ index.js
@@ -36,26 +36,53 @@ const url = require("url"); // for rounting
 /*******************************************/
 /*Server*/
 
+const replaceTemplate = (temp, product) => {
+  // regular expression: https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15080938?start=486#notes
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // variable defined
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+}
+
 // only execute once
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
-const dataObj = JSON.parse(data);
+const dataObj = JSON.parse(data); // data object (can be loop)
 
-// Create server
+// Create server & run every each of request
 const server = http.createServer((req, res)=>{
-  // console.log(req.url);
   const pathName = req.url;
 
+  // overview page
   if(pathName === '/' || pathName === '/overview') {
-    res.end("this is the OVERVIEW");
+    res.writeHead(200, {"Content-type": "text/html"});
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace("{%PRODUCT_CARD%}", cardsHtml);
+    res.end(output);
+
+  // product page
   } else if (pathName === '/product') {
     res.end("This is the PRODUCT");
-  } else if (pathName === '/api') {
 
-    res.writeHead(200, {
-      "Content-type": "application/json"  // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15080930?start=525#notes
-    });
+  // api page
+  } else if (pathName === '/api') {
+    // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15080930?start=525#notes
+    res.writeHead(200, {"Content-type": "application/json"});
     res.end(data);
+
+  // not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
